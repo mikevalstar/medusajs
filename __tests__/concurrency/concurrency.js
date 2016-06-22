2582bc09a2b502baca8f1fdee3b30df56a295bfd
Mike Valstar <mikevalstar@gmail.com>
2016-06-22 13:02:55 -0400

    added rejection cases

--- __tests__/concurrency/concurrency.js
+++ __tests__/concurrency/concurrency.js
@@ -34,4 +34,37 @@ describe('concurrency', () => {
 
   });
 
+  pit('Only calls the function once, and rejects both', () => {
+
+    return new Promise(resolve => {
+
+      jest.useRealTimers();
+
+      var calledCount = 100;
+
+      var incrementor = function(res, rej) {
+        setTimeout(()=> {
+          rej(calledCount += 1);
+        }, 1000);
+      };
+
+      var t1 = Medusa.get('sampleConcurrentrej', incrementor, 1500)
+        .then(res => expect(res).toEqual(9999))
+        .catch(res => expect(res).toEqual(101));
+
+      var t2 = Medusa.get('sampleConcurrentrej', incrementor, 1500)
+        .then(res => expect(res).toEqual(9999))
+        .catch(res => expect(res).toEqual(101));
+
+      //console.error("here 2", setTimeout.mock, jest.useRealTimers);
+      jest.runAllTimers();
+
+      Promise.all([t1, t2]).then(() => {
+        resolve(true);
+      });
+
+    });
+
+  });
+
 });
